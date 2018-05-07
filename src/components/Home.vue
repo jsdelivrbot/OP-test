3a4caf00371139db4d5c936448af47f63eeae804
Eléonore Voisin <eleonore.voisin@lagardere-active.com>
2018-05-07 15:56:29 +0200

    add zoom item

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,19 +11,25 @@
                   <span class="header-name"> Ryo Takemasa </span>
                 </h1>
             </div>
-          <div v-for="item in items">
-            <router-link :to="{ name: 'Post', params: { id: item.id } }">
-              <div class="grid-item">
-                <img :src="url + item.title"
+          <div v-for="(item, index) in items">
+            <div :key="index" @click="zoomItem(index)" v-bind:class="currentItem == index ? 'active' : ''">
+              <div  v-bind:class="currentItem == index ? 'grid-item grid-item--active' : 'grid-item'">
+                <img :src="imageUrl + item.title"
                       class="grid-item-img"
                       :alt="item.title">
-                <span class="grid-item-title">
+                <span v-if="currentItem != index" class="grid-item-title">
                   {{ item.title.length > 25 ?
                       item.title.substr(0,25) + '...'
                       : item.title }}
                 </span>
+                <span v-else class="grid-item-title grid-item-title--active">
+                    {{ item.title }}
+                    <span class="grid-item-body">
+                        {{ item.body }}
+                     </span>
+                </span>
               </div>
-            </router-link>
+            </div>
           </div>
         </masonry>
       </div>
@@ -32,8 +38,8 @@
 </template>
 
 <script>
+
   import Vue from 'vue'
-  import Post from '@/components/Post'
   import VueMasonry from 'vue-masonry-css'
 
   Vue.use(VueMasonry)
@@ -43,9 +49,11 @@
 
     data () {
       return {
+        open: false,
         items: [],
-        url: "https://source.unsplash.com/random?"
-      }
+        imageUrl: "https://source.unsplash.com/random?",
+        currentItem: null
+        }
     },
     methods: {
         fetchItems() {
@@ -53,6 +61,9 @@
                 .then(stream => stream.json())
                 .then(response => this.items = response)
                 .catch(error => console.error(error))
+        },
+        zoomItem(index) {
+            this.currentItem === index ? this.currentItem = null : this.currentItem = index
         }
       },
     mounted() {
@@ -79,7 +90,7 @@
       font-size: 1.5rem;
       text-transform: uppercase;
       padding: 15px;
-      margin: 0;
+      margin: 10px;
     }
 
     .header-name {
@@ -89,7 +100,7 @@
     }
 
     .grid {
-      margin-top: 50px;
+      margin-top: 20px;
     }
 
     .grid-item {
@@ -98,6 +109,7 @@
       margin: 10px 0;
       background-color: $c-darken;
       transition: all 0.5s ease;
+      cursor: pointer;
     }
 
     .grid-item-title {
@@ -121,7 +133,7 @@
       width: 100%;
     }
 
-    .grid-item:hover {
+    .grid-item:not(.grid-item--active):hover {
       padding: 10px;
       & > .grid-item-title {
         bottom: 30px;
@@ -129,4 +141,46 @@
         font-weight: 600;
       }
     }
+
+    .hidden {
+        display: none;
+    }
+
+    .active {
+       position: fixed;
+       width: 100%;
+       height: 100vh;
+       background-color: rgba(0,0,0,0.7);
+       top: 0;
+       left: 0;
+       z-index: 100;
+    }
+
+    .grid-item--active {
+       position: relative;
+       width: auto;
+       margin-top: 40px;
+       .grid-item-img {
+            max-height: 80vh;
+            width: auto;
+            max-width: 100%;
+            margin: auto;
+       }
+    }
+
+    .grid-item-title--active {
+        position: absolute;
+        bottom: 0;
+        font-weight: 900;
+    }
+
+    .grid-item-body {
+        display: block;
+        border-top: 1px solid rgba(255,255,255,0.1);
+        padding-top: 10px;
+        font-weight: 100;
+        font-size: 1rem;
+        text-transform: none;
+    }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import Post from '@/components/Post'
 
 Vue.use(Router)
 
@@ -11,12 +10,6 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home,
-      props: true
-    },
-    {
-      path: '/post/:id',
-      name: 'Post',
-      component: Post,
       props: true
     }
   ]
